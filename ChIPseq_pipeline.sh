63b9d54259b58d38d7851740163b109fb3d8eed3
Fernandez Perez Daniel <ieo4462@cn08.cluster.loc>
2018-08-30 12:18:16 +0200

    Cleaning files, last commit before merging to master

--- ChIPseq_pipeline.sh
+++ ChIPseq_pipeline.sh
@@ -2,4 +2,4 @@
 mkdir clusterLogs # Create folder to store the cluster output and error files
 nohup snakemake -j 999 --cluster-config cluster.json --latency-wait 120 \
 --cluster "qsub -M {cluster.email} -m {cluster.EmailNotice} -N {cluster.jname} \
--l select=1:ncpus={cluster.cpu} -o {cluster.output} -e {cluster.error}" &> snakemake.log&
+-l select=1:ncpus={cluster.cpu} -o {cluster.output} -e {cluster.error}" &>> snakemake.log&
