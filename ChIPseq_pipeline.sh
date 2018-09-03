a2f0504a324027357365e5e8feedc574ef251b2a
Fernandez Perez Daniel <ieo4462@cn09.cluster.loc>
2018-09-03 18:17:53 +0200

    Updates on singularity, moved some parameters to config.yml

--- ChIPseq_pipeline.sh
+++ ChIPseq_pipeline.sh
@@ -1,5 +1,6 @@
 # Usage --> ./ChIPseq_pipeline.sh       (the samples.json file must be created first, also remember to check the cluster and config files to put everything to your needs
+snakemake --unlock
 mkdir clusterLogs # Create folder to store the cluster output and error files
-nohup snakemake -j 999 --cluster-config cluster.json --latency-wait 120 \
+nohup snakemake -j 999 --cluster-config cluster.json --latency-wait 120 --use-singularity --singularity-args "-B /hpcnfs" \
 --cluster "qsub -M {cluster.email} -m {cluster.EmailNotice} -N {cluster.jname} \
 -l select=1:ncpus={cluster.cpu} -o {cluster.output} -e {cluster.error}" &>> snakemake.log&
