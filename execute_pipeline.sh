f2f3f5c52ff0fce48086a1e8a047d9c25e5ffa04
Fernandez Perez Daniel <ieo4462@ieo.it>
2020-06-03 14:02:26 +0200

    Change clusterlog path

--- execute_pipeline.sh
+++ execute_pipeline.sh
@@ -1,4 +1,3 @@
 # Usage --> ./execute_pipeline.sh  <rules> (can be nothing or a rule defined in the snakefile without wildcards in the input files)
-mkdir -p .clusterLogs # Create folder to store the cluster output and error files
 snakemake -j 1 --unlock
 nohup snakemake --profile workflow/snakemake_profile "$@" &>> snakemake.log&
