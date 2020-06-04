ab369b770edaace475e68503751a020751d597be
Fernandez Perez Daniel <ieo4462@ieo.it>
2020-06-04 14:33:36 +0200

    Create clusterLogs folder

--- execute_pipeline.sh
+++ execute_pipeline.sh
@@ -1,3 +1,4 @@
 # Usage --> ./execute_pipeline.sh  <rules> (can be nothing or a rule defined in the snakefile without wildcards in the input files)
+mkdir -p results/.clusterLogs
 snakemake -j 1 --unlock
 nohup snakemake --profile workflow/snakemake_profile "$@" &>> results/snakemake.log&
